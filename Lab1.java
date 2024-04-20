e1142fb0ccc5ec8f2d44527867ff8a741b675d1d
nhoxstin999 <nhoxstin858@gmail.com>
2024-04-20 11:04:28 +0700

    v4

--- Lab1.java
+++ Lab1.java
@@ -1,13 +1,15 @@
 public class Lab1 {
     public static void main(String[] args) {
-    //System.out.println(new Question1(12356).SumFirstAndLAstDigit());
+    //System.out.println(new Question1().SumFirstAndLAstDigit());
 
-    //System.out.println(new Question2(4,2,5).MinNumber());
+    //System.out.println(new Question2().MinNumber());
 
-    /*Question3 question3 =new Question3(5);
+    /*Question3 question3 =new Question3();
     question3.HailStone();*/
 
-     Question4 question4 = new Question4(5);
-     System.out.println(question4.SumAllEvenN());
+//  Question4 question4 = new Question4();
+//  System.out.println(question4.SumAllEvenN());
+
+
     }
 }
---
+++ Question5.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Question5 {
+    private int n;
+
+    public Question5() {
+    }
+    public Question5(int n) {
+        this.n = n;
+    }
+    public boolean RemoveFirstElement() {
+        
+    }
+}
